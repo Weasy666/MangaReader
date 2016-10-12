4106d614d7f94be7412e29cbb3bfd1b2fda32c83
Daniel Wiesenberg <weasy@hotmail.de>
2016-10-12 23:20:53 +0200

    added template10

---
+++ MangaReader_MVVM/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using MangaReader_MVVM.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace MangaReader_MVVM.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
---
+++ MangaReader_MVVM/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using MangaReader_MVVM.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace MangaReader_MVVM.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
